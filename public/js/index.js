70692030af8ac938a29bf482e1714d65aded8ee4
nirajx1d <32599550+nirajx1d@users.noreply.github.com>
2018-12-18 16:07:44 +0530

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        // console.log('scroll to bottom');
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
     // socket.emit('createMessage', {
@@ -22,6 +40,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 //new location message
@@ -35,6 +54,7 @@ socket.on('newLocationMessage', function(message) {
     });
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 })
 
 // socket.emit('createMessage',{
